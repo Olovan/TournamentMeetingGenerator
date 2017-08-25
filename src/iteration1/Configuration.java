471a8f53edc00127d20845258a852139b98d1e57
Olovan <micahsmith.92@gmail.com>
2017-08-25 13:34:17 -0400

    Added Configuration Class

---
+++ src/iteration1/Configuration.java
@@ -0,0 +1,13 @@
+package iteration1;
+
+//Contains options for easy passing as a function argument
+//Initialized with Config.txt file
+public class Configuration {
+	public int numberOfSectionalHosts = 32;
+	public int numberOfRegionalHosts = 16;
+	public int numberOfSemiStateHosts = 4;
+	
+	//TODO:MICAH
+	public void loadFromFile(String filename) {
+	}
+}
