bd69edb104664ad1f73b03ea4fbe59d4d8c630e2
Olovan <micahsmith.92@gmail.com>
2017-08-30 01:26:10 -0400

    Added config file loading

--- src/iteration1/Configuration.java
+++ src/iteration1/Configuration.java
@@ -1,18 +1,66 @@
 package iteration1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 //Contains options for easy passing as a function argument
 //Initialized with Config.txt file
 public class Configuration {
 	public int numberOfSectionalHosts = 32;
 	public int numberOfRegionalHosts = 16;
 	public int numberOfSemiStateHosts = 4;
-	
+
 	public boolean useBreakpoints = false;
+	public int numberOfClasses = 4;
 	public int[] classBreakpoints = {200, 400, 800};
 	public int[] minTeamsPerHost { 8, 2, 4 };
 
-	
-	//TODO:MICAH
+	public Configuration(String filename) {
+		loadFromFile(filename);
+	}
+
+
 	public void loadFromFile(String filename) {
+		BufferedReader reader = BufferedReader( FileReader(filename) );
+
+		String line;
+		while((line = reader.readLine()) != null)
+		{
+			String[] tokens = line.split(" ");
+
+			switch(tokens[0].toUpperCase())
+			{
+				case "NUMBER_OF_SECTIONAL_HOSTS":
+					numberOfSectionalHosts = Integer.parseInt(tokens[1]);
+					break;
+				case "NUMBER_OF_REGIONAL_HOSTS":
+					numberOfRegionalHosts = Integer.parseInt(tokens[1]);
+					break;
+				case "NUMBER_OF_SEMI-STATE_HOSTS":
+					numberOfSemiStateHosts = Integer.parseInt(tokens[1]);
+					break;
+				case "USE_BREAKPOINTS":
+					useBreakpoints = Boolean.parseBoolean(tokens[1]);
+					break;
+				case "NUMBER_OF_CLASSES":
+					numberOfClasses = Integer.parseInt(tokens[1]);
+					break;
+				case "CLASS_BREAKPOINTS":
+					classBreakpoints = new int[numberOfClasses - 1];
+					for(int i = 1; i < numberOfClasses; i++) {
+						classBreakpoints[i - 1] = tokens[i];
+					}
+					break;
+				case "MINIMUM_TEAMS_PER_HOST":
+					for(int i = 0; i < 3; i++) {
+						minTeamsPerHost[i] = tokens[i + 1];
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		reader.close();
 	}
 }
