159c6faa71d5f459f5840eb190edde197eb0dd3d
arnora01 <31223445+arnora01@users.noreply.github.com>
2017-09-02 12:08:29 -0400

    Minor updates to MapAndListGUI
    +added skeleton methods for modifyMenu ActionListeners
    +SaveListener() now asks the user to input a Tournament name, then renames current tournament before outputting the text file or Tournament object.

--- src/iteration1/MapAndListGUI.java
+++ src/iteration1/MapAndListGUI.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class MapAndListGUI extends JFrame {
     // object the GUI is displaying
-    private Tournament currentTournament;
+    protected Tournament currentTournament;
 
     // default file output path
     private final File OUT_PATH = new File("output/");
@@ -31,7 +31,7 @@ public class MapAndListGUI extends JFrame {
     private JPanel mapPanel;
     private JPanel listPanel;
     
-    // TODO Ryan
+    // TODO Ryan or Quentin
     // Constructor
     public MapAndListGUI(File selectedFile) {
     	super();
@@ -46,7 +46,7 @@ public class MapAndListGUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(1,2));
         
-        // create menus
+        // create file menu
         menuBar = new JMenuBar();
         fileMenu = new JMenu("File");
         generateMenuItem = new JMenuItem("Generate New");
@@ -61,13 +61,15 @@ public class MapAndListGUI extends JFrame {
         exitMenuItem = new JMenuItem("Exit");
         exitMenuItem.setToolTipText("Close application");
         exitMenuItem.addActionListener(new ExitListener());
-        modifyMenu = new JMenu("Modify");
-        // [ADD ACTION LISTENER]
-        changeHostItem = new JMenuItem("Change Host(s)");
-        changeParticipantItem = new JMenuItem("Change Participant(s)");
-        // [ADD ACTION LISTENER]
         
-        // add menus to frame
+        // create modify menu
+        modifyMenu = new JMenu("Modify");
+        changeHostItem = new JMenuItem("Change Host(s)");
+        changeHostItem.addActionListener(new ChangeHostListener());
+        changeParticipantItem = new JMenuItem("Change Participant(s)");
+        changeParticipantItem.addActionListener(new ChangeParticipantListener());
+        
+        // add menus to bar then bar to window
         fileMenu.add(generateMenuItem);
         fileMenu.add(saveMenuItem);
         fileMenu.add(loadMenuItem);
@@ -85,7 +87,7 @@ public class MapAndListGUI extends JFrame {
         add(listPanel);
     }
     
-    // TODO Ryan
+    // TODO Ryan or Quentin
     public boolean generateTournament(File selectedFile) {
         // discern file Enrollment(.csv) or Saved Tournament(.ser)
         // if .csv call ReadSchoolFile()
@@ -94,7 +96,7 @@ public class MapAndListGUI extends JFrame {
         return true;
     }
     
-    // TODO Ryan
+    // TODO Ryan or Quentin
     private boolean loadSavedTournament(File saveFile) {
         Tournament savedTourney = null; // [TEMP]
 
@@ -104,7 +106,7 @@ public class MapAndListGUI extends JFrame {
         return true;
     }
     
-    // TODO Ryan 
+    // TODO Ryan or Quentin
     public class GenerateListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             // new GenerateMenu()
@@ -112,12 +114,33 @@ public class MapAndListGUI extends JFrame {
     }
     
     // TODO Ryan 
+    // Output torunament table to txt and serilize current Tournament for later loading.
     // [TEST!!!]
     public class SaveListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             // Successful saving of tournament schedule or meet data.
             Boolean result;
-
+            
+            // Get tournament name from user and update
+            String newName = (String)JOptionPane.showInputDialog(null,
+                                                                 "What would you like to call this current tournament setup?",
+                                                                 "Please Name Tournament",
+                                                                 JOptionPane.QUESTION_MESSAGE);
+            // rename current Tournament if valid entry
+            if ((newName != null) && (newName.length() > 0)) {
+                // rename Tournament
+                currentTournament.tournamentName = newName;
+            }
+            else {
+                // exit method
+                JOptionPane.showMessageDialog(null,
+                                              "Name is invalid, unable to save.",
+                                              "Output Warning",
+                                              JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            
+            
             // Make an output file if needed
             if (!OUT_PATH.exists()) {
                 OUT_PATH.mkdir();
@@ -156,16 +179,33 @@ public class MapAndListGUI extends JFrame {
         }
     }
     
-    // TODO Ryan 
+    // TODO Ryan or Quentin
     public class LoadListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             // new TournamentMenu()
         }
     }
     
+    // Exit Application
     public class ExitListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             System.exit(0);
+        }
+    }
+    
+    // TODO Ryan or Quentin
+    public class ChangeHostListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // have user modify the hosts for torunaments from a new window
+            // make sure to save modficications to the current tournament object!
+        }
+    }
+    
+    // TODO Ryan or Quentin
+    public class ChangeParticipantListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // have user modify the competitors in each of the meets from a new window
+            // make sure to save modficications to the current tournament object!
         }
     }
 }
