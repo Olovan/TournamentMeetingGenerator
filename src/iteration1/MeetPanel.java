3da0e9211520688d588ca5faef562d50c7272cd2
arnora01 <31223445+arnora01@users.noreply.github.com>
2017-09-03 00:47:35 -0400

    Changed layout of MeetPnl for improved button display.
    -needs scroll wheel
    -top btns either need to be centered or made larger

--- src/iteration1/MeetPanel.java
+++ src/iteration1/MeetPanel.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class MeetPanel extends JPanel{
-    private GridLayout layout = new GridLayout(0,1);
-    private Dimension size = new Dimension(115,115);
+    private BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+    private Dimension pnlSize = new Dimension(117,1000);
+    private Font font1 = new Font("Arial", Font.BOLD, 14);
+    private Font font2 = new Font("Arial", Font.PLAIN, 14);
     
     private MapPanel map;
     private Match[] meetsArr;
@@ -19,7 +21,7 @@ public class MeetPanel extends JPanel{
     public MeetPanel(String meetCategory, Match[] listOfMeets, MapPanel map) {
         // formating
         setLayout(layout);
-        setPreferredSize(size);
+        setPreferredSize(pnlSize);
         
         // prepare map for drawing
         this.map = map;
@@ -30,6 +32,7 @@ public class MeetPanel extends JPanel{
         // construct and add title button
         allMeetsBtn = new JButton(meetCategory);
         allMeetsBtn.addActionListener(new AllMeetsBtnListener());
+        allMeetsBtn.setFont(font1);
         add(allMeetsBtn);
         
         // construct meet buttons, catalog name in hostArr, and add to frame
@@ -40,6 +43,7 @@ public class MeetPanel extends JPanel{
             hostName = meetsArr[i].host.toString();
             meetBtn = new JButton(hostName);
             meetBtn.addActionListener(new MeetBtnListener());
+            meetBtn.setFont(font2);
             
             hostArr.add(hostName);
             add(meetBtn);
@@ -53,8 +57,7 @@ public class MeetPanel extends JPanel{
         }
     }
     
-    // TODO Ryan
-    // [TEST]
+    // Send selected Match for mapPanel display
     public class MeetBtnListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
